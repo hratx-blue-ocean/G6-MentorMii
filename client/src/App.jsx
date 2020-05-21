7fe55fecf5bc594f7e96e38a7296292cfc85d354
D9SE <daltonsoohoo9@gmail.com>
2020-05-21 17:52:26 -0500

    bleh

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -26,7 +26,7 @@ export default class App extends Component {
   render() {
     return (
       <div>
-        <div className="headerBorder"> </div>
+        <div className="headerBorder"></div>
         <Header />
 
         <SignIn />

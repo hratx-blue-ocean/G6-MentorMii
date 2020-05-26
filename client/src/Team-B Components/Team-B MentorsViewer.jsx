113b36e9fea563e66a140e0fa9b6d227da5a9a64
Marc Rodriguez <Marcanthonyrodriguez95@gmail.com>
2020-05-26 12:58:37 -0500

    May 26th Update 1258

--- client/src/Team-B Components/Team-B MentorsViewer.jsx
+++ client/src/Team-B Components/Team-B MentorsViewer.jsx
@@ -1,36 +1,23 @@
 /* eslint-disable */
 import React from "react";
 import "../Styles/Team-B MentorsViewer.css";
-import logo from "../DummyData/12055105.jpg"; // Tell webpack this JS file uses this image
 
 class MentorsViewer extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-  render(props) {
-    return (
-      <div>
-        MentorsViewer
-      {this.props.Mentors}
-        <div className="Team-B-MentorsViewer">
-          <div onClick={this.props.MentorsViewerFunc} className="Team-B-Mentor">
-            {/* <img className="Team-B-ReviewImages" src={logo} alt="Logo" /> */}
-            <div></div>
-          </div>
-          <div>
-          </div>
-          <br></br>
-        </div>
+function MentorsViewer(props) {
+  return (
+    <div>
+      <div>MentorsViewer</div>
+      <div onClick={props.MentorsViewerFunc} className="Team-B-MentorsViewer">
+        {props.Mentors}
       </div>
-    );
-  }
+    </div>
+  );
 }
 export default MentorsViewer;
 
-
 //DummyData:
-{/* <br></br>
+{
+  /* <br></br>
           <div onClick={this.props.MentorsViewerFunc} className="Team-B-Mentor">
             <img className="Team-B-ReviewImages" src={logo} alt="Logo" />
             <div>Chief Wiggums</div>
@@ -59,4 +46,5 @@ export default MentorsViewer;
           <div onClick={this.props.MentorsViewerFunc} className="Team-B-Mentor">
             <img className="Team-B-ReviewImages" src={logo} alt="Logo" />
             <div>Officer Barbrady</div>
-          </div> */}
+          </div> */
+}

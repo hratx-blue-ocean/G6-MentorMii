598da1e82da995419960fac1815ac99129f876bc
Paul Prokopy <58863283+Proko8@users.noreply.github.com>
2020-05-27 12:35:08 -0500

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,16 +11,11 @@ export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      seaCreatures: []
+    
     };
-    this.api = `http://localhost:8000/api/example`;
+    
   }
   componentDidMount() {
-    fetch(this.api)
-      .then(res => res.json())
-      .then(seaCreatures => {
-        this.setState({ seaCreatures: seaCreatures.data });
-      });
   }
 
   render() {
